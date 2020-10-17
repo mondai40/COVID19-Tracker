5ec6f107c468fc7c27f3b4bec15d1ee06685cd9b
40 daimon <tvwmdaimon40@gmail.com>
2020-10-17 14:35:56 -0700

    create a header with material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState(['Japan', 'Korea', 'Canada']);
+
   return (
     <div className="app">
-      <h1>COVID-19 TRACKER</h1>
-      {/* Header */}
-      {/* Title + select input dropdown field */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select>
+            {countries.map((country) => {
+              return <MenuItem value={country}>{country}</MenuItem>;
+            })}
+            {/* <MenuItem value="world">World</MenuItem>
+            <MenuItem value="Japan">Japan</MenuItem>
+            <MenuItem value="USA">USA</MenuItem>
+            <MenuItem value="Canada">Canada</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* infobox */}
       {/* infobox */}
