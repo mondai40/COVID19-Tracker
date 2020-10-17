09bebfc97449443ad147fcc741effe45bb5a7766
40 daimon <tvwmdaimon40@gmail.com>
2020-10-17 14:57:43 -0700

    create useEffect to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 import './App.css';
 
+// retrieve data from
+// https://disease.sh/
+// https://disease.sh/docs/
+
 function App() {
   const [countries, setCountries] = useState(['Japan', 'Korea', 'Canada']);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    };
+    getCountriesData();
+  }, [countries]);
 
   return (
     <div className="app">
