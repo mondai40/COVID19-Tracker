5c13ab54fd3e5fb994e2cdccd2e1da68677345cf
40 daimon <tvwmdaimon40@gmail.com>
2020-10-18 16:48:50 -0700

    implement fetch data for LineGraph

--- src/LineGraph.jsx
+++ src/LineGraph.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 
 function LineGraph() {
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    (async function getDataForChart() {
+      await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('test', data);
+        });
+    })();
+  }, []);
+
   return (
     <div>
       <h1>I'm a graph</h1>
