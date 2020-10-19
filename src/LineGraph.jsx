a56059ed92422dbcc2cc25008e76f16aa9654391
40 daimon <tvwmdaimon40@gmail.com>
2020-10-18 21:47:18 -0700

    add data format function

--- src/LineGraph.jsx
+++ src/LineGraph.jsx
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 
+const buildChartData = (data, casesType = 'cases') => {
+  const chartData = [];
+  let lastDataPoint;
+  Object.entries(data[casesType]).forEach(([date, cases]) => {
+    if (lastDataPoint) {
+      const newDataPoint = {
+        x: date,
+        y: cases - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
+    }
+    lastDataPoint = cases;
+  });
+  return chartData;
+};
+
 function LineGraph() {
-  const [chartData, setChartData] = useState({});
+  const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
     (async function getDataForChart() {
       await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
         .then((response) => response.json())
         .then((data) => {
-          console.log('test', data);
+          setChartData(buildChartData(data));
         });
     })();
   }, []);
@@ -18,6 +34,7 @@ function LineGraph() {
     <div>
       <h1>I'm a graph</h1>
       {/* <Line data options /> */}
+      {console.log('hre', chartData)}
     </div>
   );
 }
